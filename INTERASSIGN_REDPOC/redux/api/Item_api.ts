68d3cf9064de368c025054f68920e2898dfd1c6b
Sahil Chauksey <113386193+wazzer1803@users.noreply.github.com>
2024-05-16 15:09:37 +0530

    Update Item_api.ts

--- INTERASSIGN_REDPOC/redux/api/Item_api.ts
+++ INTERASSIGN_REDPOC/redux/api/Item_api.ts
@@ -15,7 +15,7 @@ export const ItemApi_RTK_QUERY = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl:
       // "http://localhost:4000/api/v1",
-      "https://interassignment-redpos.vercel.app/api/v1",
+      "https://interass-ignmen-redpos.vercel.app/api/v1",
      
     prepareHeaders(headers) {
       const token = localStorage.getItem("token");
